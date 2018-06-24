4c5446d3b2fa17c560d7e3ff318a3b302d34cccf
EduardaGoulart <goularteduarda.a@gmail.com>
2018-06-24 13:55:30 -0300

    código refatorado

--- knn.py
+++ knn.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
+# Lê um arquivo CSV
+from csv import reader
+# Gera números aleatórios
 from random import random
-import math
-import operator
+# Possui a operação de raíz quadrada
+from math import sqrt
+# Retorna o valor em uma posição
+from operator import itemgetter
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -21,6 +25,7 @@ class Knn:
         file = input()
         if file == 'iris' or file == "Iris" or file == '1':
             return 'iris.data'
+
         self.escolhe_data()
 
     def carrega_arquivo(self, split, conjunto_treinados, conjunto_teste):
@@ -32,15 +37,15 @@ class Knn:
         """
         filename = self.escolhe_data()
         with open(filename, 'rt') as arquivo_csv:
-            lines = csv.reader(arquivo_csv)
-            conjunto_dados = list(lines)
+            conjunto_dados = list(reader(arquivo_csv))
             for x in range(len(conjunto_dados) - 1):
                 for y in range(4):
                     conjunto_dados[x][y] = float(conjunto_dados[x][y])
                 """
-                Gera um valor randômico, se for menor que a porcentagem de elementos utilizados, será adicionado em um 
-                conjunto de dados que serão treinados. 
-                Caso o valor gerado seja maior que o split, será adicionado no cojunto de dados teste
+                Gera um valor randômico que define a quantidade de dados utilizados como treinamento e como teste,
+                desta forma, com a geração aleatória, aumenta a curva de aprendizagem.
+                Se o valor for menor que a porcentagem de split, será adicionado em um conjunto de dados que serão 
+                treinados. Caso o valor gerado seja maior que o split, será adicionado no cojunto de dados teste
                 """
                 if random() < split:
                     conjunto_treinados.append(conjunto_dados[x])
@@ -50,51 +55,66 @@ class Knn:
     @staticmethod
     def distancia_euclidiana(valor1, valor2, tamanho):
         """
-        Essa função calcula a distância euclidiana entre dois conjuntos de dados
+        Função calcula a distância euclidiana entre dois conjuntos de dados
+        Com o tamanho do dado analisado, eleva cada posição (cada dados, valor1 e valor2, são vetores) dele ao quadrado
+        e adiciona em um vetor a distância será a raíz quadrada das somas dos valores do vetor
         """
-        dist = [pow((valor1[x] - valor2[x]), 2) for x in range(tamanho)]
-        return math.sqrt(sum(dist))
+        return sqrt(sum([pow((valor1[x] - valor2[x]), 2) for x in range(tamanho)]))
 
-    def separa_vizinhos(self, conjunto_treinados, instancia_teste, k):
-        distancias = []
-        tamanho = len(instancia_teste) - 1
-        for x in range(len(conjunto_treinados)):
-            dist = self.distancia_euclidiana(instancia_teste, conjunto_treinados[x], tamanho)
-            distancias.append((conjunto_treinados[x], dist))
-        distancias.sort(key=operator.itemgetter(1))
-        vizinhos = []
-        for x in range(k):
-            vizinhos.append(distancias[x][0])
+    def separa_vizinhos(self, conjunto_treinados, conjunto_teste, k):
+        """
+        Dado um conjunto de valores treinados e um conjunto de teste, é calculado sua distância eucliadiana
+        A quantidade de vizinhos que serão selecionados (k), determina quantos valores dentro do raio da distância
+        serão utilizados. 
+        """
+        tamanho = len(conjunto_teste) - 1
+
+        """
+        para um conjunto de treinamento e teste, é calculada a distância euclidiana entre eles, adicionados a um vetor
+        que é ordenado de forma a pegar os primeiros k vinhos do conjunto de treinamento
+        """
+        distancias = [(conjunto_treinados[i], self.distancia_euclidiana(conjunto_teste, conjunto_treinados[i], tamanho))
+                      for i in range(len(conjunto_treinados))]
+        # ordena o vetor
+        distancias.sort(key=itemgetter(1))
+        # pega os primeiros k vizinhos do conjunto de treinamento
+        vizinhos = [distancias[i][0] for i in range(k)]
+
         return vizinhos
 
     @staticmethod
-    def responsavel(vizinhos):
+    def votos_majoritarios(vizinhos):
         votos = {}
         for x in range(len(vizinhos)):
-            response = vizinhos[x][-1]
-            if response in votos:
-                votos[response] += 1
+            if vizinhos[x][-1] not in votos:
+                votos[vizinhos[x][-1]] = 1
             else:
-                votos[response] = 1
-        ordena_votos = sorted(votos.items(), key=operator.itemgetter(1), reverse=True)
-        return ordena_votos[0][0]
+                votos[vizinhos[x][-1]] += 1
+        return (sorted(votos.items(), reverse=True, key=itemgetter(1)))[0][0]
 
     @staticmethod
     def calcula_acerto(conjunto_teste, nome_dado):
         correto = 0
-        for x in range(len(conjunto_teste)):
-            if conjunto_teste[x][-1] == nome_dado[x]:
+        for i in range(len(conjunto_teste)):
+            if nome_dado[i] == conjunto_teste[i][-1]:
                 correto += 1
         return (correto / float(len(conjunto_teste))) * 100.0
 
     def run(self):
+        """
+        a variável k representa a quantidade de vizinhos mais próximos que será utilizada na pesquisa de
+        k-vizinhos. Para classificar o conjunto de vizinhos mais próximos e tomar voto da maioria, seleciona-se um
+        conjunto de tamanho ímpar, para evitar que haja empates.
+        """
         while True:
-            k = int(input("Digite um valor para K: "))
+            k = int(input("Digite um valor ímpar para K: "))
             if k % 2 != 0:
-                break;
+                break
 
         conjunto_treinados = []
         conjunto_teste = []
+        
+        # Carrega os dados do arquivo, separando nas duas listas (treino e teste) de acordo com o split
         self.carrega_arquivo(0.67, conjunto_treinados, conjunto_teste)
 
         print('Conjunto treinado: {}'.format(str(len(conjunto_treinados))))
@@ -102,18 +122,21 @@ class Knn:
 
         nome_dado = []
 
-        for x in range(len(conjunto_teste)):
-            vizinhos = self.separa_vizinhos(conjunto_treinados, conjunto_teste[x], k)
-            result = self.responsavel(vizinhos)
+        for i in range(len(conjunto_teste)):
+            vizinhos = self.separa_vizinhos(conjunto_treinados, conjunto_teste[i], k)
+            result = self.votos_majoritarios(vizinhos)
             nome_dado.append(result)
-            if result == conjunto_teste[x][-1]:
-                print('Valor obtido: {}, valor real: {} -> ACERTOU'.format(str(result), str(conjunto_teste[x][-1])))
+            if result == conjunto_teste[i][-1]:
+                print('Valor obtido: {} || valor real: {} -> ACERTOU'.format(str(result), str(conjunto_teste[i][-1])))
             else:
-                print('Valor obtido: {}, valor real: {} -> ERROU'.format(str(result), str(conjunto_teste[x][-1])))
+                print('Valor obtido: {} || valor real: {} -> ERROU'.format(str(result), str(conjunto_teste[i][-1])))
         acerto = self.calcula_acerto(conjunto_teste, nome_dado)
         print('Taxa de acerto: {0:.3f}%'.format(acerto))
 
 
 if __name__ == '__main__':
+    """
+    Executa o problema
+    """
     obj = Knn()
     obj.run()
